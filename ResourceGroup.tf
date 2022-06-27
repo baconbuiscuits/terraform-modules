41ad56d22424869864a8ac9f8ac5d69b330296b9
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-27 10:04:35 +0200

    fix: change iterator

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "the_resource_group" {
   for_each = var.input_data
   iterator = RG
-  name     = RG.value.name
-  location = RG.value.location
-  tags     = RG.value.tags
+  name     = RG.name
+  location = RG.location
+  tags     = RG.tags
 }
