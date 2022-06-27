816f827fb02a1c0adfebfdc198c0878e3f52eb7a
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-27 10:07:58 +0200

    fix: rewrite RG logic

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -1,7 +1,9 @@
 resource "azurerm_resource_group" "the_resource_group" {
   for_each = var.input_data
   iterator = RG
-  name     = RG.name
-  location = RG.location
-  tags     = RG.tags
+  content {
+  name     = RG.value.name
+  location = RG.value.location
+  tags     = RG.value.tags
+  }
 }
