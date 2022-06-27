2d1786cd8b2c200c4ad152f960804562d791fa5a
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-27 15:16:01 +0200

    feat: start work on VM module

---
+++ VirtualMachine.tf
@@ -0,0 +1,63 @@
+# resource "azurerm_network_interface" "the_nic" {
+#   name                = "example-nic"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.example.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
+
+# resource "azurerm_linux_virtual_machine" "the_linux_VM" {
+#   name                = "example-machine"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   size                = "Standard_F2"
+#   admin_username      = "adminuser"
+#   network_interface_ids = [
+#     azurerm_network_interface.example.id,
+#   ]
+
+#   admin_ssh_key {
+#     username   = "adminuser"
+#     public_key = file("~/.ssh/id_rsa.pub")
+#   }
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "16.04-LTS"
+#     version   = "latest"
+#   }
+# }
+
+# resource "azurerm_windows_virtual_machine" "the_windows_VM" {
+#   name                = "example-machine"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   size                = "Standard_F2"
+#   admin_username      = "adminuser"
+#   admin_password      = "P@$$w0rd1234!"
+#   network_interface_ids = [
+#     azurerm_network_interface.example.id,
+#   ]
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -15,3 +15,23 @@ variable "ResourceGroup_input_data" {
     }
   }
 }
+
+# variable "VirtualMachine_input_data" {
+#   description = "Virtual machine input data"
+#   type = map(object({
+#     name = string
+#     resource_group_name = string
+#     location = string
+#     size = string
+#     admin_username = string
+    
+
+
+
+
+
+
+#     tags     = optional(map(string))
+#   }))
+  
+# }
