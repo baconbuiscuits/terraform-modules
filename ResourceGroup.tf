3e8988a6702f83dc58c1f4cad626cdb87b27f49c
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-27 13:28:13 +0200

    fix: revert to variable

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -1,9 +1,7 @@
 resource "azurerm_resource_group" "the_resource_group" {
   for_each = var.ResourceGroup_input_data
-  iterator = RG
-
-  name     = RG[each.key].name
-  location = RG[each.key].location
-  tags     = RG[each.key].tags
+  name     = var.ResourceGroup_input_data[each.key].name
+  location = var.ResourceGroup_input_data[each.key].location
+  tags     = var.ResourceGroup_input_data[each.key].tags
 
 }
