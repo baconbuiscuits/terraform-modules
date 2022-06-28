29b8817e87ff557db54fbefe048eae620661e1dd
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-28 11:36:48 +0200

    feat: add firewall module

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "ResourceGroup_input_data" {
-  description = "Resource group input data"
-  type = map(object({
-    name     = string
-    location = string
-    tags     = optional(map(string))
-  }))
-  default = {
-    "key" = {
-      location = "westeurope"
-      name     = "default_name"
-      tags = {
-        "no-tags" = "no-tags"
-      }
-    }
-  }
-}
-
 # variable "VirtualMachine_input_data" {
 #   description = "Virtual machine input data"
 #   type = map(object({
