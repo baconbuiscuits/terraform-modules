00530f962c5e032c532b9361a3cf9fbcec8fb25d
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-29 10:18:01 +0200

    fix: change output logic for RGs

--- out_ResourceGroup.tf
+++ out_ResourceGroup.tf
@@ -1,9 +1,12 @@
 output "rg_name" {
-  value = azurerm_resource_group.the_resource_group[*].name
+  value = [for x in azurerm_resource_group.the_resource_group : x.name]
+  #value = azurerm_resource_group.the_resource_group[*].name
 }
 output "rg_id" {
-  value = azurerm_resource_group.the_resource_group[*].id
+  value = [for x in azurerm_resource_group.the_resource_group : x.id]
+  #value = azurerm_resource_group.the_resource_group[*].id
 }
 output "rg_location" {
-  value = azurerm_resource_group.the_resource_group[*].location
+  value = [for x in azurerm_resource_group.the_resource_group : x.location]
+  #value = azurerm_resource_group.the_resource_group[*].location
 }
