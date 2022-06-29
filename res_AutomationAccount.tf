9b86ee2ae04e419a79a72b7f536e58307b5ec5ee
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-29 11:48:23 +0200

    fix: rewrite logic

--- res_AutomationAccount.tf
+++ res_AutomationAccount.tf
@@ -1,20 +1,20 @@
 resource "azurerm_automation_account" "the_automation_account" {
   for_each                      = var.AutomationAccount_input_data
   iterator = automationaccount
-  name                          = automationaccount[each.key].name
-  location                      = automationaccount[each.key].location
-  resource_group_name           = automationaccount[each.key].rg_name
-  sku_name                      = automationaccount[each.key].sku_name == null ? "Basic" : automationaccount[each.key].sku_name
-  public_network_access_enabled = automationaccount[each.key].public_network_access_enabled == null ? true : false
+  name                          = automationaccount.value.name
+  location                      = automationaccount.value.location
+  resource_group_name           = automationaccount.value.rg_name
+  sku_name                      = automationaccount.value.sku_name == null ? "Basic" : automationaccount.value.sku_name
+  public_network_access_enabled = automationaccount.value.public_network_access_enabled == null ? true : false
   dynamic "identity" {
-    for_each = automationaccount[each.key].identity
+    for_each = automationaccount.value.identity
     iterator = identity
     content {
-      type         = identity[each.key].type
-      identity_ids = identity[each.key].type != "SystemAssigned" ? identity[each.key].identity_ids : null
+      type         = identity.value.type
+      identity_ids = identity.value.type != "SystemAssigned" ? identity.value.identity_ids : null
 
     }
   }
 
-  tags = var.AutomationAccount_input_data[each.key].tags
+  tags = var.AutomationAccount_input_data.value.tags
 }
--- res_ResourceGroup.tf
+++ res_ResourceGroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "the_resource_group" {
   for_each = var.ResourceGroup_input_data
   iterator = resourcegroup
-  name     = resourcegroup[each.key].name
-  location = resourcegroup[each.key].location
-  tags     = resourcegroup[each.key].tags
+  name     = resourcegroup.value.name
+  location = resourcegroup.value.location
+  tags     = resourcegroup.value.tags
 
 }
