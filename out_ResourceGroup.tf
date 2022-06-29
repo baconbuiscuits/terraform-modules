4b4d40cd5233daf05c10e011dd0e46d703971809
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-29 09:58:58 +0200

    fix: test output logic

--- out_ResourceGroup.tf
+++ out_ResourceGroup.tf
@@ -1,9 +1,9 @@
 output "rg_name" {
-  value = azurerm_resource_group.the_resource_group.*.name
+  value = azurerm_resource_group.the_resource_group[*].name
 }
 output "rg_id" {
-  value = azurerm_resource_group.the_resource_group.*.id
+  value = azurerm_resource_group.the_resource_group[*].id
 }
 output "rg_location" {
-  value = azurerm_resource_group.the_resource_group.*.location
+  value = azurerm_resource_group.the_resource_group[*].location
 }
