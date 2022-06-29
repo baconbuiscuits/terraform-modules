fde916f6ea8fc400445a93d2641c99d3feda59a6
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-29 11:56:05 +0200

    fix: revert logic

--- res_ResourceGroup.tf
+++ res_ResourceGroup.tf
@@ -1,9 +1,7 @@
 resource "azurerm_resource_group" "the_resource_group" {
   for_each = var.ResourceGroup_input_data
-  content {
-    iterator = resourcegroup
-    name     = resourcegroup.value.name
-    location = resourcegroup.value.location
-    tags     = resourcegroup.value.tags
-  }
+  name     = var.ResourceGroup_input_data[each.key].name
+  location = var.ResourceGroup_input_data[each.key].location
+  tags     = var.ResourceGroup_input_data[each.key].tags
+
 }
