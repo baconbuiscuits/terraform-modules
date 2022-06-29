cb27546a410900d8c9865fb8d5fbbea3784dc2a3
Jacek Kossowski <jacek.kossowski@evry.com>
2022-06-29 08:59:00 +0200

    fix: remove FW rule logic

--- variables_FirewallRule.tf
+++ variables_FirewallRule.tf
@@ -1,81 +1,81 @@
-###################################
-### RULES COLLECTION VARIABLES  ###
-###################################
+# ###################################
+# ### RULES COLLECTION VARIABLES  ###
+# ###################################
 
-variable "name" {
-  description = "firewall policy rule collection name"
-  default = null
-  type        = string
-}
-variable "rule_collection_priority" {
-  description = "priority of this rule collection"
-  default = null
-  type        = number
-}
+# variable "name" {
+#   description = "firewall policy rule collection name"
+#   default = null
+#   type        = string
+# }
+# variable "rule_collection_priority" {
+#   description = "priority of this rule collection"
+#   default = null
+#   type        = number
+# }
 
-###################################
-### APPLICATION RULES VARIABLES ###
-###################################
-variable "app_rules" {
-  description = "app rules collection"
-  type = map(object({
-    rc_name  = string
-    priority = optional(number)
-    action   = optional(string)
-    rules = map(object({
-      name              = string
-      source_addresses  = list(string)
-      destination_fqdns = list(string)
-      protocols = map(object({
-        protocol = string
-        port     = number
-      }))
-    }))
-  }))
-  default = {}
-}
+# ###################################
+# ### APPLICATION RULES VARIABLES ###
+# ###################################
+# variable "app_rules" {
+#   description = "app rules collection"
+#   type = map(object({
+#     rc_name  = string
+#     priority = optional(number)
+#     action   = optional(string)
+#     rules = map(object({
+#       name              = string
+#       source_addresses  = list(string)
+#       destination_fqdns = list(string)
+#       protocols = map(object({
+#         protocol = string
+#         port     = number
+#       }))
+#     }))
+#   }))
+#   default = {}
+# }
 
-###################################
-###   NETWORK RULES VARIABLES   ###
-###################################
-variable "net_rules" {
-  description = "net rules collection"
-  type = map(object({
-    rc_name  = string
-    priority = optional(number)
-    action   = optional(string)
-    rules = map(object(
-      {
-        name                  = string
-        protocols             = list(string)
-        source_addresses      = list(string)
-        destination_addresses = list(string)
-        destination_ports     = list(string)
-    }))
-  }))
-  default = {}
-}
+# ###################################
+# ###   NETWORK RULES VARIABLES   ###
+# ###################################
+# variable "net_rules" {
+#   description = "net rules collection"
+#   type = map(object({
+#     rc_name  = string
+#     priority = optional(number)
+#     action   = optional(string)
+#     rules = map(object(
+#       {
+#         name                  = string
+#         protocols             = list(string)
+#         source_addresses      = list(string)
+#         destination_addresses = list(string)
+#         destination_ports     = list(string)
+#     }))
+#   }))
+#   default = {}
+# }
 
 
-###################################
-###     NAT RULES VARIABLES     ###
-###################################
+# ###################################
+# ###     NAT RULES VARIABLES     ###
+# ###################################
 
-variable "nat_rules" {
-  description = "nat rules collection"
-  type = map(object({
-    rc_name  = string
-    priority = optional(number)
-    action   = optional(string)
-    rules = map(object({
-      name                = string
-      protocols           = list(string)
-      source_addresses    = list(string)
-      destination_address = string
-      destination_ports   = list(string)
-      translated_address  = string
-      translated_port     = string
-    }))
-  }))
-  default = {}
-}
+# variable "nat_rules" {
+#   description = "nat rules collection"
+#   type = map(object({
+#     rc_name  = string
+#     priority = optional(number)
+#     action   = optional(string)
+#     rules = map(object({
+#       name                = string
+#       protocols           = list(string)
+#       source_addresses    = list(string)
+#       destination_address = string
+#       destination_ports   = list(string)
+#       translated_address  = string
+#       translated_port     = string
+#     }))
+#   }))
+#   default = {}
+# }
